4c7fdc226fe719bd1dceadd596e38e4e85eadd08
Ruan-Franklin <ruanfranklinmengo@gmail.com>
2024-09-02 08:14:56 -0300

    :sparkles: feat: adicionando valores de entrada para o grafo valorado

--- exemplos/TadGrafoPython/matriz_adjacencia.py
+++ exemplos/TadGrafoPython/matriz_adjacencia.py
@@ -9,7 +9,11 @@ class Grafo:
         print("Número de vértices" f' {self.vertices}')
         for i in range(self.vertices):
             print(self.grafo[i])
-GrafoA = Grafo(4)
-GrafoA.adicionaAresta(1, 2, 5)
-GrafoA.adicionaAresta(4,1, 7)
+quantidade_vertices = int(input("Digite a quantidade de vértices do grafo: "))
+GrafoA = Grafo(quantidade_vertices)
+for i in range(quantidade_vertices):
+    verticeA = int(input("Digite o vértice de origem: "))
+    verticeB = int(input("Digite o vértice de destino: "))
+    valor = int(input("Digite o valor da aresta: "))
+    GrafoA.adicionaAresta(verticeA, verticeB, valor)
 GrafoA.mostrarMatriz()
